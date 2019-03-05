52479d5b0460bab8419310b4ecafca0a802a7e26
Landon Brenner <landon.brenner@gmail.com>
2019-03-04 18:12:48 -0800

    removed unused code and components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-import './App.css';
 
 //import Sandbox from './components/sandbox';
 //import Autocomplete from './components/autocomplete';
@@ -10,16 +8,6 @@ import Dashboard from './components/containers/dashboard';
 class App extends Component {
 
   render() {
-    /*
-    const suggestions = [
-      "Cornelius",
-      "Ethel",
-      "Fred",
-      "George",
-      "Harriet"
-    ]
-    */
-
     return (
       <>
         <Dashboard />
